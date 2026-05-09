2325ddd9566703f84edfd871e3e6598c91c8f44d
Arbe <bektemir1102@gmail.com>
2026-05-09 15:58:25 +0600

    edited endpoints for JWT token

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
+from pwdlib import PasswordHash
 
 from app.auth.schemas import UserSchema, UserLoginSchema, UserResponseSchema
 from app.auth.models import User as UserModel
 from app.database import SessionLocal, get_db
+from app.auth.jwt import create_token, get_current_user
 
 router = APIRouter(
     prefix='/auth',
-    tags=['Авторизация']
+    tags=['Authentication']
 )
-#I connect the JWT token tomorrow.
+
+pwd = PasswordHash.recommended()
 
 #temporarily to check which users are in the database
 @router.get('/all')
@@ -19,44 +22,53 @@ def all_users(db: Session = Depends(get_db)):
 
 @router.post('/reg')
 def registration(user: UserSchema, db: Session = Depends(get_db)):
+    existing_user = db.query(UserModel).filter(
+        UserModel.username == user.username
+    ).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=404,
+            detail='The user already exists'
+        )
+
+    hashed_password = pwd.hash(user.password)
+
     new_user = UserModel(**user.model_dump())
+    new_user.password = hashed_password
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
-@router.post('/log', response_model=UserLoginSchema)
+@router.post('/log')
 def login(user: UserLoginSchema, db: Session = Depends(get_db)):
     db_user = db.query(UserModel).filter(
         UserModel.username == user.username,
         UserModel.password == user.password
     ).first()
-    if not db_user:
+    if not db_user or pwd.verify(user.password, db_user.password):
         raise HTTPException(
             status_code=404,
             detail='Incorrect login or password'
         )
-    return user
 
-@router.post('/refresh')
-def refresh_token():
-    pass
+    token = create_token(db_user.id)
+    return {'token': token}
 
-@router.put('/change_password')
-def change_password():
-    pass
-
-@router.get('/my_profile', response_model=UserResponseSchema)
-def profile(db: Session = Depends(get_db)):
-    user_profile = db.query(UserModel).first()
+@router.get('/my_profile')
+def profile(db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
+    user_profile = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if not user_profile:
+        raise HTTPException(status_code=404, detail='User Not Found')
     return user_profile
 
-@router.put('/my_profile/edit/{user_id}', response_model=UserResponseSchema)
-def edit_profile(user_id: int, user: UserResponseSchema, db: Session = Depends(get_db)):
+@router.put('/my_profile/edit', response_model=UserResponseSchema)
+def edit_profile(user: UserResponseSchema, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
     db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
 
     if not db_user:
-        raise HTTPException(status_code=404, detail='Пользователь не найден')
+        raise HTTPException(status_code=404, detail='User Not Found')
 
     for key, value in user.model_dump().items():
         setattr(db_user, key, value)
@@ -66,8 +78,8 @@ def edit_profile(user_id: int, user: UserResponseSchema, db: Session = Depends(g
     return db_user
 
 
-@router.delete('/my_profile/del/{user_id}')
-def del_user(user_id: int, db: Session = Depends(get_db)):
+@router.delete('/my_profile/del')
+def del_user(db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
     user = db.query(UserModel).filter(UserModel.id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail='User Not Found')
