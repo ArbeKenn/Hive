4531e7f2bc2d22a30cc71d6570f57c7b0d4eda9d
Arbe <bektemir1102@gmail.com>
2026-05-10 22:38:51 +0600

    I've implemented basic CRUD endpoints (GET, POST, PUT, DELETE)

--- app/posts/router.py
+++ app/posts/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.posts.models import Post as PostModel
@@ -17,3 +17,16 @@ def all_posts(db: Session = Depends(get_db)):
 @router.post('/new_post')
 def create_post():
     return 'yo'
+
+@router.put('/edit')
+def edit_post(db: Session = Depends(get_db)):
+    pass
+
+@router.delete('/del/{post_id}')
+def del_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(PostModel).filter(PostModel.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=404, detail='Post Not Found')
+    db.delete(post)
+    db.commit()
+    return {'message': 'Account deleted'}
